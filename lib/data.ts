92f5fe29be8ccdb6035c499fa5f50abe7881f23d
abhi-duk <abhi.krishnan@duk.ac.in>
2026-05-18 15:18:16 +0530

    ch

--- lib/data.ts
+++
@@ -1,4 +0,0 @@
-export const SHOWS = [
-  { showId: 'SHOW_EMP_001', movieTitle: 'L2: Empuraan', theatreId: 'KSFDC_SREE_TVM', theatreName: 'KSFDC Sree, TVM', timeHint: '18:30' },
-  { showId: 'SHOW_OD_001', movieTitle: 'Officer on Duty', theatreId: 'KSFDC_SREE_TVM', theatreName: 'KSFDC Sree, TVM', timeHint: '21:15' }
-];
